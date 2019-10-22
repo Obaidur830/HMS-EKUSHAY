2e039ac493029c3bb451bb988cd6dac9de1edf9f
Obaidur <bsse0830@iit.du.ac.bd>
2019-10-22 20:36:18 +0600

    refresh routes issue solved successfully

--- src/app/root/navbar/navbar.component.ts
+++ src/app/root/navbar/navbar.component.ts
@@ -13,132 +13,144 @@ import { RootService } from '../services/root.service';
 import { SecurityService } from 'src/app/core/security-service/security.service';
 
 @Component({
-	selector: 'app-navbar',
-	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
-	isHandset$: Observable<boolean> = this.breakpointObserver
-		.observe(Breakpoints.Handset)
-		.pipe(map((result) => result.matches));
+    isHandset$: Observable<boolean> = this.breakpointObserver
+        .observe(Breakpoints.Handset)
+        .pipe(map((result) => result.matches));
 
-	title: string;
-	sidebar;
-	Username: string;
-	// $username: Observable<any>;
-	menuItems;
-	selectedRow: number;
-	_unsubscribeAll: Subject<any>;
+    title: string;
+    sidebar;
+    Username: string;
+    // $username: Observable<any>;
+    menuItems;
+    selectedRow: number;
+    // tslint:disable-next-line: variable-name
+    _unsubscribeAll: Subject<any>;
 
-	// $menuIndex = this.menuIndex.asObservable();
-	constructor(
-		private breakpointObserver: BreakpointObserver,
-		private aut: AuthenticationService,
-		private router: Router,
-		private corequery: QueryDatabaseService,
-		private sharedService: SharedService,
-		private rootService: RootService,
-		private security: SecurityService
-	) {
-		this._unsubscribeAll = new Subject();
-	}
+   // $menuIndex = this.menuIndex.asObservable();
+    constructor(
+        private breakpointObserver: BreakpointObserver,
+        private aut: AuthenticationService,
+        private router: Router,
+        private corequery: QueryDatabaseService,
+        private sharedService: SharedService,
+        private rootService: RootService,
+        private security: SecurityService
+    ) {
+        this._unsubscribeAll = new Subject();
+    }
 
-	ngOnInit() {
-		this.initiateVariables();
-		this.setUsername();
-		this.selectedRow = 1;
-		this.route(defaultConst.sidebar[1].url);
-		//this.route(defaultConst.sidebar[1].url);
-		// console.log(this.isHandset$.subscribe(res=>console.log(res)));
-		// console.log(this.aut.getCurrentUser());
-	}
+    ngOnInit() {
+        this.initiateVariables();
+        this.setUsername();
 
-	initiateVariables() {
-		this.title = defaultConst.siteName.name;
-		this.menuItems = defaultConst.menu;
+       // this.route(defaultConst.sidebar[1].url);
 
-		this.makeSideBar();
+    }
 
-		this.rootService.$Username.subscribe((res) => {
-			this.Username = res;
-		});
+    initiateVariables() {
+        this.title = defaultConst.siteName.name;
+        this.menuItems = defaultConst.menu;
 
-		this.rootService.$menuIndex.subscribe(res => {
-			this.selectedRow = res;
-		});
-	}
+        this.makeSideBar();
 
-	makeSideBar() {
-		this.security.isAdmin().pipe(first()).subscribe(res => {
-			if (res) {
-				this.sidebar = defaultConst.sidebar;
-			} else {
-				this.sidebar = defaultConst.sidebarforcustomersupplier;
-			}
-		});
+        this.rootService.$Username.subscribe((res) => {
+             this.Username = res;
+        });
+
+        this.rootService.$menuIndex.subscribe(res => {
+           this.selectedRow = res;
+        });
+    }
+
+    makeSideBar() {
+        this.security.isAdmin().pipe(first()).subscribe(res => {
+            if (res) {
+                this.sidebar = defaultConst.sidebar;
+            } else {
+                this.sidebar = defaultConst.sidebarforcustomersupplier;
+            }
+            this.checkAndSetRow();
+        });
 
 
-	}
+    }
+    checkAndSetRow() {
+        const currentUrl = this.router.url;
+        let count = 0;
+        for (const i of this.sidebar) {
+            if (currentUrl === `/${i.url}`) {
+                this.selectedRow = count;
+                break;
+            }
+            count += 1;
+        }
+    }
 
-	setUsername() {
-		this.corequery.getLoggedInUserID().pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
-			if (res) {
-				this.getUserName(res).pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
-					this.Username = res;
-					this.rootService.$Username.next(this.Username);
-				});
-			} else {
-				this.Username = Roles.Anonymous;
-				this.rootService.$Username.next(Roles.Anonymous);
-			}
-		});
-	}
+    setUsername() {
+        this.corequery.getLoggedInUserID().pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
+            if (res) {
+                // tslint:disable-next-line: no-shadowed-variable
+                this.getUserName(res).pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
+                    this.Username = res;
+                    this.rootService.$Username.next(this.Username);
+                });
+            } else {
+                this.Username = Roles.Anonymous;
+                this.rootService.$Username.next(Roles.Anonymous);
+            }
+        });
+    }
 
-	getUserName(uid): Observable<any> {
-		let personInfo: CustomerUserInformation;
+    getUserName(uid): Observable<any> {
+       let personInfo: CustomerUserInformation;
 
-		return new Observable((observer) => {
-			this.corequery
-				.getSingleData(Entities.Person, uid)
-				.pipe(takeUntil(this._unsubscribeAll))
-				.subscribe((res) => {
-					personInfo = res;
-					observer.next(personInfo && personInfo.name ? personInfo.name : null);
-				});
-		});
-	}
+       return new Observable((observer) => {
+            this.corequery
+                .getSingleData(Entities.Person, uid)
+                .pipe(takeUntil(this._unsubscribeAll))
+                .subscribe((res) => {
+                    personInfo = res;
+                    observer.next(personInfo && personInfo.name ? personInfo.name : null);
+                });
+       });
+    }
 
-	logout() {
-		this.aut.signOut();
-		this.showLogoutSnackbar();
-	}
+    logout() {
+        this.aut.signOut();
+        this.showLogoutSnackbar();
+    }
 
-	showLogoutSnackbar() {
-		this.sharedService.openSnackBar({
-			data: {
-				message: 'Logged out successfully',
-				isAccepted: true
-			},
-			duration: 3,
-			horizontalPosition: 'right',
-			verticalPosition: 'top',
-			panelClass: ['default-snackbar']
-		});
-	}
+    showLogoutSnackbar() {
+        this.sharedService.openSnackBar({
+            data: {
+                message: 'Logged out successfully',
+                isAccepted: true
+            },
+            duration: 3,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            panelClass: ['default-snackbar']
+        });
+    }
 
-	ngOnDestroy() {
-		this._unsubscribeAll.next();
-		this._unsubscribeAll.complete();
-		this._unsubscribeAll.unsubscribe();
-	}
-	route(url) {
-		this.router.navigateByUrl(url);
-		if (url === defaultConst.menu.profile.url) {
-			this.rootService.$menuIndex.next(-1);
-		}
-	}
-	selectRow(index) {
-		this.selectedRow = index;
-		// console.log(this.selectedRow);
-	}
+    ngOnDestroy() {
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+        this._unsubscribeAll.unsubscribe();
+    }
+    route(url) {
+        this.router.navigateByUrl(url);
+        if (url === defaultConst.menu.profile.url) {
+            this.rootService.$menuIndex.next(-1);
+        }
+    }
+    selectRow(index) {
+        this.selectedRow = index;
+
+    }
 }
