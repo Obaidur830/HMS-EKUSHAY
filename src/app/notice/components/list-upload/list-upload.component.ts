5672479773edff4624d49914a1732574d502b044
Obaidur <bsse0830@iit.du.ac.bd>
2019-11-18 12:29:54 +0600

    added download file successfully

--- src/app/notice/components/list-upload/list-upload.component.ts
+++ src/app/notice/components/list-upload/list-upload.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { UploadFileService } from 'src/app/notice-board/service/upload-file.service';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { DatePipe } from '@angular/common';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-list-upload',
@@ -10,7 +11,7 @@ import { DatePipe } from '@angular/common';
 })
 export class ListUploadComponent implements OnInit {
 
-  constructor(private uploadService: UploadFileService, private datePipe: DatePipe) { }
+  constructor(private uploadService: UploadFileService, private datePipe: DatePipe, private storage: AngularFireStorage) { }
   listData: MatTableDataSource<any>;
   displayedColumns: string[] = ['name', 'type', 'modified', 'noticeFor', 'publisherName', 'actions'];
   @ViewChild(MatSort) sort: MatSort;
@@ -43,8 +44,53 @@ export class ListUploadComponent implements OnInit {
     window.open(url, '_blank', 'noopener');
 
   }
-  goDownloadURL(url) {
-    window.open(url);
-
+  goDownloadURL(url, fileName) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = (event) => {
+      /* Create a new Blob object using the response
+      *  data of the onload object.
+      */
+      const blob = new Blob([xhr.response]);
+      const a: any = document.createElement('a');
+      a.style = 'display: none';
+      document.body.appendChild(a);
+      const url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    };
+    xhr.open('GET', url);
+    xhr.send();
   }
+
+  // public downloadFile(): void {
+  //   const storageRef = this.storage.ref('/uploads').child('/1573735295095_New Text Document.txt')
+  //     .getDownloadURL();
+  //     console.log(storageRef);
+  //     //.then((url) => {
+  //       const xhr = new XMLHttpRequest();
+  //       xhr.responseType = 'blob';
+  //       xhr.onload = (event) => {
+  //         /* Create a new Blob object using the response
+  //         *  data of the onload object.
+  //         */
+  //         const blob = new Blob([xhr.response], { type: 'text' });
+  //         const a: any = document.createElement('a');
+  //         a.style = 'display: none';
+  //         document.body.appendChild(a);
+  //         const url = window.URL.createObjectURL(blob);
+  //         a.href = url;
+  //         a.download = 'filename';
+  //         a.click();
+  //         window.URL.revokeObjectURL(url);
+  //       };
+  //       xhr.open('GET', url);
+  //       xhr.send();
+  //     }).catch( (error) => {
+  //       // Handle any errors
+  //       console.log(error);
+  //     });
+  // }
 }
